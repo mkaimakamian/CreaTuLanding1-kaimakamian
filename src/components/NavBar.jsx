fe700293712c73703a74f785d5e364a8d01869c3
Kokumo <mkc@outlook.com.ar>
2025-05-10 21:28:09 -0300

    Agregado react bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
 import CartWidget from "./CartWidget"
 
 const NavBar = ({ logo }) => {
-    return <nav>
-        <img src={logo} alt="Vending Machine logo" height={24} width={24} />
-        <div>
-            <p>Catálogo</p>
-            <p>Nosotros</p>
-            <p>Contacto</p>
-        </div>
-
-        <CartWidget />
-    </nav>
+    return <Navbar expand="lg" className="bg-primary-subtle p-0">
+        <Container fluid>
+            <Navbar.Brand href="#"><img src={logo} alt="Vending Machine logo" height="24" width="24" /></Navbar.Brand>
+            <Nav.Link href="#action1">Catálogo</Nav.Link>
+            <Nav.Link href="#action2">Nosotros</Nav.Link>
+            <Nav.Link href="#action3">Contacto</Nav.Link>
+            <CartWidget />
+        </Container>
+    </Navbar>
 }
 
 export default NavBar
