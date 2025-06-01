4d5b6cd5ce2f949ff2941763317b690b56f4f997
Kokumo <mkc@outlook.com.ar>
2025-05-31 22:59:51 -0300

    Working filter

--- src/apis/catalog.tsx
+++ src/apis/catalog.tsx
@@ -3,16 +3,23 @@ import axios from "axios";
 export const GetItems = async () => {
 
     const data = await axios.get(
-        "https://fakestoreapi.com/products/category/electronics"
+        "https://fakestoreapi.com/products"
     ).then(response => response.data);
 
     return data;
 }
 
 export const GetItem = async (id) => {
-
     const data = await axios.get(
         `https://fakestoreapi.com/products/${id}`
+    ).then(response => response.data);
+
+    return data;
+}
+
+export const GetItemCategories = async () => {
+    const data = await axios.get(
+        `https://fakestoreapi.com/products/categories`
     ).then(response => response.data);
 
     return data;
